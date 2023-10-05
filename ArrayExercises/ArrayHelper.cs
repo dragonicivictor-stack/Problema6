f4c7a4b724a7d20a75f04d406f8eb7fae1092d51
Florin Lazar <florin.lazar.it2014@gmail.com>
2023-10-05 23:20:53 +0300

    exercises with arrays

---
+++ ArrayExercises/ArrayHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayExercises
+{
+    public static class ArrayHelper
+    {
+        public static int Min(int[] array)
+        {
+            if (array is null)
+            {
+                throw new ArgumentNullException(nameof(array));
+            }
+
+            if (array.Length == 0)
+            {
+                throw new ArgumentException(
+                    "Array must have at least 1 element",
+                    nameof(array));
+            }
+
+            int min = array[0];
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] < min)
+                {
+                    min = array[i];
+                }
+            }
+
+            return min;
+        }
+    }
+}
---
+++ ArrayExercises/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ArrayExercises
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = ConsoleHelper.ReadNumber("The size of the array=", 3, 0);
+            int[] array = new int[size];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                int element = ConsoleHelper.ReadNumber($"element[{i}]=", 3, 0);
+                array[i] = element;
+            }
+
+            ConsoleHelper.PrintArray("Array=", array);
+
+            int minValue = ArrayHelper.Min(array);
+            Console.WriteLine($"Min={minValue}");
+        }
+    }
+}
